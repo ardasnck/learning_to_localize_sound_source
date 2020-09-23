1c5e3cbeca69a4e1a316297a559e2f2c827a3db4
Arda Senocak <arda.senocak@gmail.com>
2020-09-24 00:25:14 +0900

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,40 @@
+import csv
+import numpy as np
+import pdb
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+class Logger(object):
+
+    def __init__(self, path, header):
+        self.log_file = open(path, 'w')
+        self.logger = csv.writer(self.log_file, delimiter='\t')
+        self.logger.writerow(header)
+        self.header = header
+
+    def __del(self):
+        self.log_file.close()
+
+    def log(self, values):
+        write_values = []
+        for col in self.header:
+            assert col in values
+            write_values.append(values[col])
+        self.logger.writerow(write_values)
+        self.log_file.flush()
